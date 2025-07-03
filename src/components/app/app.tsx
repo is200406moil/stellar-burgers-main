137b6e8d3e1d637560ac802cf3cae7a69be3cf26
is200406moil <is200406moil@gmail.com>
2025-07-03 14:23:44 +0300

    feat: add react-router-dom routing and modal routes, add ProtectedRoute stub

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,148 @@
-import { ConstructorPage } from '@pages';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  Navigate
+} from 'react-router-dom';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+import { OrderInfo } from '../order-info/order-info';
+import { IngredientDetails } from '../ingredient-details/ingredient-details';
+import { Modal } from '../modal/modal';
 import '../../index.css';
 import styles from './app.module.css';
-
 import { AppHeader } from '@components';
+import { FC } from 'react';
+
+// Заглушка ProtectedRoute
+const ProtectedRoute: FC<{ children: React.ReactNode }> = ({ children }) => (
+  // TODO: реализовать проверку авторизации
+  // const isAuth = ...
+  // return isAuth ? <>{children}</> : <Navigate to="/login" />;
+  <>{children}</>
+);
+const ModalSwitch = () => {
+  const location = useLocation();
+  // @ts-ignore
+  const state = location.state as { backgroundLocation?: Location };
+  const backgroundLocation = state && state.backgroundLocation;
+
+  return (
+    <>
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRoute>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      {/* Модалки */}
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal
+                title='Информация о заказе'
+                onClose={() => window.history.back()}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal
+                title='Детали ингредиента'
+                onClose={() => window.history.back()}
+              >
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute>
+                <Modal
+                  title='Информация о заказе'
+                  onClose={() => window.history.back()}
+                >
+                  <OrderInfo />
+                </Modal>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      )}
+    </>
+  );
+};
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <Router>
+    <div className={styles.app}>
+      <AppHeader />
+      <ModalSwitch />
+    </div>
+  </Router>
 );
 
 export default App;
